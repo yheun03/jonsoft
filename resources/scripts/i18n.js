02da4ebde52a3b11a46257eda2857b0c02c17154
Nyoung_Ni <71180175+yheun03@users.noreply.github.com>
2025-08-06 13:36:20 +0900

    헤더의 i18n 버튼 클릭 이벤트 추가

--- resources/scripts/i18n.js
+++ resources/scripts/i18n.js
@@ -1,2 +1,15 @@
 // 다국어 지원을 위한 스크립트
-// 향후 다국어 기능 구현 예정 
+$(function() {
+    const setLang = lang => {
+        $('html').attr('lang', lang);
+        $('.i18n li').removeClass('active')
+            .filter((_, li) => $(li).find('.btn').data('lang') === lang)
+            .addClass('active');
+    };
+    $('.i18n .btn').on('click', function() {
+        const lang = $(this).data('lang');
+        setLang(lang);
+        localStorage.setItem('selectedLang', lang);
+    });
+    setLang(localStorage.getItem('selectedLang') || $('html').attr('lang') || 'ko');
+});
